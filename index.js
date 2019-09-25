c91ff2d2c2ae64ee62041be38dc819782134dfe8
pantera <alex5612+gitkraken@hotmail.fr>
2019-09-25 22:33:49 +0200

    use dash-button lib instead of node-dash-button

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 const 	config		= require('config'),
 		// Dash Button Setup
-		dashButton	= require('node-dash-button'),
-		dash		= dashButton(config.get('dash.mac'), null, null, 'all'),
+		DashButton	= require('dash-button'),
+		dash		= new DashButton(config.get('dash.mac')),
 		// Light Setup
 		LifxClient	= require('node-lifx').Client,
 		lifx		= new LifxClient(),
@@ -11,20 +11,20 @@ const 	config		= require('config'),
 		;
 		
 lifx.init({
-	lights : [lifxIp]
+	lights : [ lifxIp ]
 });
 
 // Dash Button
-dash.on("detected", () =>
-{
-	lifx.light(lifxIp).getPower((err, lightState) => {
-		if (lightState) {
-			lifx.light(lifxIp).off();
-		} else {
-			lifx.light(lifxIp).on();
-		}
-		console.log('Switch Button to: ' + (!lightState ? 'on' : 'off'));	
-	});
+dash.addListener(async () => {
+	let lightState = await lifx.light(lifxIp).getPower();
+
+	if (lightState) {
+		lifx.light(lifxIp).off();
+	} else {
+		lifx.light(lifxIp).on();
+	}
+
+	console.log('Switch Button to: ' + (!lightState ? 'on' : 'off'));
 });
 
 console.log('Light Ready');
